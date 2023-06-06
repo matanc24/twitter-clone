499a898ab95657bc7615999ec65d29fb242b2f7c
Matan Cohen <matanc24@gmail.com>
2023-06-06 13:22:23 +0300

    newtweet

---
+++ src/components/NewTweetForm.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./Button";
+
+export function NewTweetForm() {
+  return (
+    <form action="" className="flex flex-col gap-2 border-b px-4 py-2">
+      <div className="flex gap-4">
+        {/* <ProfileImage src="#url"/> */}
+        <textarea
+          className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
+          placeholder="What's Happening?"
+        />
+      </div>
+      <Button>Tweet</Button>
+    </form>
+  );
+}
